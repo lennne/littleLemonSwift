857a7c354caa3efe2d860bc2075372ef8f7d0e32
lennne <kobiansah@gmail.com>
2025-09-07 11:37:49 -0230

    update: implemented @state and @binding variable for enabling communication between two views

--- littleLemonSwift/ContentView.swift
+++ littleLemonSwift/ContentView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var personCount: Int = 1
+    @State var showLogo: Bool = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MyView()
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            Stepper {
+                Text("Reservation for: \(personCount)")
+            } onIncrement: {
+                personCount += 1
+            } onDecrement: {
+                personCount = (personCount == 1) ? 1 : personCount - 1
+            }
+            
+            if showLogo {
+                LittleLemonLogo(bindingVariable: $showLogo)
+            }
+            
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct LittleLemonLogo: View {
+    @Binding var bindingVariable: Bool
+    var body: some View{
+        VStack{
+            Image("LittleLemonLogo")
+            Button(action: {
+                bindingVariable.toggle()
+            }, label: {
+                Text("Toggle Logo Visibility").font(.title2)
+            })
+        }
+    }
+}
+
+struct ContentView_Previwes: PreviewProvider {
+    static var previews: some View{
+        ContentView()
+    }
 }
---
+++ littleLemonSwift/MyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyView.swift
+//  littleLemonSwift
+//
+//  Created by Joseph Acquah on 2025-09-02.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text("Little Lemon")
+    }
+}
+
+#Preview {
+    MyView()
+}

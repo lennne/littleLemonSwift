7db14f4f49502a24a2b1f97672a6619e3295144d
lennne <kobiansah@gmail.com>
2025-09-14 12:24:01 -0230

    update: implemented both hierarchical and tabbed navigations

--- littleLemonSwift/ContentView.swift
+++ littleLemonSwift/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State var inputValue: String = "" //control variable
     var body: some View {
 
-                CombiningStacks()
+        TabViewPractice()
     }
        
 }
--- littleLemonSwift/littleLemonSwiftApp.swift
+++ littleLemonSwift/littleLemonSwiftApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct littleLemonSwiftApp: App {
     var body: some Scene {
         WindowGroup {
-            ImageView()
+            ContentView()
         }
     }
 }
